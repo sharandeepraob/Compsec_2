7e7349bec4dc098564d656bc52c2f8fa547f57b9
sharandeepbejjanki <sharandeebejjanki20@gmail.com>
2023-09-08 23:06:38 -0500

    eleventh commit

--- executable/executable/settings.py
+++ executable/executable/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
