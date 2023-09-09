58c2b3a49f759bd5336174f6238dd4161b8264f9
sharandeepbejjanki <sharandeebejjanki20@gmail.com>
2023-09-08 22:33:55 -0500

    fifth commit

--- executable/executable/settings.py
+++ executable/executable/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nzknde#&gcndtqgu(#o2(-yo3ratmvgdtz081)$!)stjq6#f!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['compsec-f9dc69b10b03.herokuapp.com']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
